0d536e20c6e9e27f46664566b561daa1fa3b4d27
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-11-09 01:16:58 +0600

    Little changes on app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Header of material ui</h2>
+        <h2>Header of material ui</h2>
+        <h2>Header of material ui</h2>
+        <h2>Header of material ui</h2>
+        <h2>Header of material ui</h2>
+        <p>da </p>
+        <p></p>
       </header>
     </div>
   );
